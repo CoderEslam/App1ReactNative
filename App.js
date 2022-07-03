8fbff0140e4f544099fce0694251633d4f717e48
eslam <eslamfgazy600@gmail.com>
2022-07-04 00:33:51 +0200

    Update ui

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import {Button, StyleSheet, Text, TextInput, ToastAndroid, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
     return (
         <View style={styles.appContiner}>
             {/* <View>
@@ -22,49 +26,51 @@ export default function App() {
                 <Button title={"click me".trim()}/>
             </View>
             <Text> Hello Ghazy</Text>*/}
-            <View style={styles.inputCuntinner}>
-                <TextInput onTouchStart={s => {
-                    ToastAndroid.showWithGravityAndOffset(
-                        'dsbfbf',
-                        ToastAndroid.LONG,
-                        ToastAndroid.BOTTOM,
-                        25,
-                        50
-                    );
-                }} style={styles.textInput} placeholder={'your course goal'}/>
-                <Button title={'add Goal'} onPress={event => {
-                    ToastAndroid.showWithGravityAndOffset(
-                        'advsargawb' + event.eventPhase,
-                        ToastAndroid.LONG,
-                        ToastAndroid.BOTTOM,
-                        25,
-                        50
-                    );
-                }}/>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder={'your course goal'} onChangeText={goalInputHandler}/>
+                <Button title={'add Goal'} onPress={addGoalHandler}/>
             </View>
-            <View>
-                <Text>
-                    List of Goals....
-                </Text>
+            <View style={styles.goalsContiner}>{courseGoals.map(s => <Text key={s}> {s}</Text>)}
             </View>
         </View>
     );
+
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoal(enteredText);
+    };
+
+    function addGoalHandler() {
+        //const setCourseGoals: (value: (((prevState: any[]) => any[]) | any[])) => void
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoal])
+    };
+
 }
 
 const styles = StyleSheet.create({
     appContiner: {
-        padding: 50
+        padding: 50,
+        flex: 1,
+        paddingHorizontal: 15 // for padding left and right only
     },
     textInput: {
         padding: 5,
-        borderColor: 'blue',
-        borderWidth: 2,
+        borderColor: 'black',
+        borderWidth: 1,
         width: '80%',
-        marginLeft: 8
+        marginLeft: 8,
     },
-    inputCuntinner: {
+    inputContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingBottom: 24,
+        borderBottomColor: 'blue',
+        borderBottomWidth: 1,
+        flex: 1
+    },
+    goalsContiner: {
+        flex: 10,
     }
     /*   container: {
          flex: 1,
@@ -95,16 +101,23 @@ const styleText = StyleSheet.create({
     }
 })
 
-const Toast = ({visible, message}) => {
-    if (visible) {
-        ToastAndroid.showWithGravityAndOffset(
-            message,
-            ToastAndroid.LONG,
-            ToastAndroid.BOTTOM,
-            25,
-            50
-        );
-        return null;
-    }
-    return null;
+function Toast(e) {
+    ToastAndroid.showWithGravityAndOffset(
+        'Eslam  => ' + e,
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        25,
+        50
+    );
 };
+
+function Alart() {
+    Alert.alert("Alert Title", "My Alert Msg", [{
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+        },
+            {text: "OK", onPress: () => console.log("OK Pressed")}
+        ]
+    );
+}
